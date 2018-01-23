47ef0a8c1d0e7690ce712e620711cc5b77291e69
Grace Newman <grace.newman@digital.hmrc.gov.uk>
2018-01-23 09:02:31 +0000

    Edited app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
